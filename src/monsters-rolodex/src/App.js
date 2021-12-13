2b33425114622d7e1cb70784fccc7d9e1ec42820
Ioan Abobului <75037928+Abobului-Ioan@users.noreply.github.com>
2021-12-13 21:21:30 +0200

    React project - Monster Rolodex

---
+++ src/monsters-rolodex/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+
+    /// this.handleChange = this.handleChange.bind(this); we use bind(this) if dot use (e) => {...} in fuction
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((Response) => Response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filtredMonster = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filtredMonster} />
+      </div>
+    );
+  }
+}
+export default App;
